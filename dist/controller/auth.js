50f4902719fae86d7d0e847785c787bb0470f27a
Aderemi Amos <aderemi.amos@decagon.dev>
2021-05-29 15:24:23 +0100

    folder structure

--- dist/controller/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.signIn = exports.signUp = void 0;
-async function signUp(req, res, next) {
-}
-exports.signUp = signUp;
-async function signIn(req, res, next) {
-}
-exports.signIn = signIn;
