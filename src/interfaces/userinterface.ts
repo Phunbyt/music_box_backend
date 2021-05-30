597dba63bb5c5c0429a251b6057b1d225856036b
Boladek <mubarak.adekunle@decagon.dev>
2021-05-30 14:18:34 +0100

    signup work

---
+++ src/interfaces/userinterface.ts
@@ -0,0 +1,9 @@
+export interface user {
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+    gender: string;
+    email: string;
+    password: string;
+    confirmPassword?: string;
+}
