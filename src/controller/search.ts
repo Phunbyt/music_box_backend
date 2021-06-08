f8af262700e9e982475242b7e023d62e41b67251
David Enoragbon <david.enoragbon@decagon.dev>
2021-06-08 21:37:18 +0100

    search song in playlist

--- src/controller/search.ts
+++ src/controller/search.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express';
 import axios from 'axios'
-import {PlayListModel} from '../schema/playlistsSchema';
+import {PlayListModel, SongModel} from '../schema/playlistsSchema';
 
 
 export const search = async (req: Request | any,res:Response) => {
@@ -11,56 +11,61 @@ export const search = async (req: Request | any,res:Response) => {
 
        
         //To get artist from deezer
-        const searchArtist = await axios.get(`https://api.deezer.com/search?q=artist:'${searchField}'`);
+        const searchArtist = await axios.get(`https://api.deezer.com/search/artist?q=${searchField}`);
         
         const searchArtistData = searchArtist.data.data.map((data:Record<string, any>) =>{
             return {
-                id: data.artist.id,
-                name: data.artist.name,
-                picture: data.artist.picture
+                id: data.id,
+                name: data.name,
+                picture: data.picture
                 
             }
         })
 
                 //To get album from deezer
-        const searchAlbum = await axios.get(`https://api.deezer.com/search?q=album:'${searchField}'`);
+        const searchAlbum = await axios.get(`https://api.deezer.com/search/album?q=${searchField}`);
 
         const searchAlbumData = searchAlbum.data.data.map((data:Record<string, any>) =>{
             return {
-                id: data.album.id,
-                title: data.album.title,
-                cover: data.album.cover
+                id: data.id,
+                title: data.title,
+                cover: data.cover
             }
         })
 
                 //To get playlist from userDB
-        const searchPlayList: Record<string, any> | null = await PlayListModel.find({name:{$regex:searchField, $options: '$i'}})
+        const lists:Record<string, any> =[];
+        const searchPlayList: Record<string, any> | null = await PlayListModel.find({'songs.title':{$regex:`.*${searchField}.*`, $options: '$i'}})
         let searchPublicPlayList: any = searchPlayList.filter((list: {[x:string]: string}) =>{
             return list['category'] = 'public';
         })
-
-        let playListData = searchPublicPlayList.map((data:Record<string, any>) =>{
-            return {
-                id: data._id,
-                name: data.name,
-                songs: data.songs,
-            }
+        //console.log(searchPublicPlayList)
+        let playListData = searchPublicPlayList.map((item:Record<string, any>) =>{
+           return item.songs
         })
+
+        playListData.forEach((song:Record<string, any>)=>song.forEach((el:Record<string, any>)=> lists.push({
+            _id: el._id,
+            title: el.title,
+            artist: el.artist,
+            album: el.album,
+            img: el.img
+        })))
 
         if(searchPublicPlayList.length > 0){
             playlistData = {
                 message: 'success',
-                playlist: playListData
+                playlist: lists
             };
         }else{
              playlistData = {
-                 message : "no playlist found from user db",
+                 message : "no songs in the  playlist found from user db",
                  playlist : searchPublicPlayList
              }
             };
 
             
-
+            // console.log(lists)
         return res.status(200).json({
             'message': 'success',
             'data': {
@@ -72,7 +77,7 @@ export const search = async (req: Request | any,res:Response) => {
                 
     }catch(err){
         return res.status(400).json({
-            'status':'failed ',
+            'status':'SEARCH failed ',
             'message': err.message
         });
     }
--- src/schema/playlistsSchema.ts
+++ src/schema/playlistsSchema.ts
@@ -1,37 +1,66 @@
 import mongoose from 'mongoose';
 const songsSchema = new mongoose.Schema({
     title: {
-        type: String,
-        required: true
+        type: String
     },
-}, {timestamps:true});
-const playListSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    songs: {
-        type: [songsSchema],
-    },
-    category:{
+    songId:{
         type:String,
-        required:true,
-        enum:['private', 'public'],
-        default:'private'
+        
     }, 
-    genre:{
+    artist:{
         type:String
     },
-    likes:[{
-        type: mongoose.Schema.Types.ObjectId,
-        // type:String,
-        required:true,
-        ref: 'NewUser'
-    }],
-    owner:{
+    album:{
+        type:String
+    },
+    img:{
         type:String
     }
-},{timestamps:true});
-
-export const PlayListModel = mongoose.model('playlist', playListSchema);
+}, {timestamps:true});
 export const SongModel = mongoose.model('song', songsSchema);
+const playListSchema = new mongoose.Schema(
+ {
+  name: {
+   type: String,
+   required: true,
+  },
+  songs: {
+   type: [songsSchema],
+  },
+  category: {
+   type: String,
+   required: true,
+   enum: ["private", "public"],
+   default: "private",
+  },
+  genre: {
+   type: String,
+  },
+  likes: [
+   {
+    type: mongoose.Schema.Types.ObjectId,
+    // type:String,
+    required: true,
+    ref: "NewUser",
+   },
+  ],
+  listens: [
+   {
+    type: String,
+   },
+  ],
+  likesCount: {
+   type: Number,
+   default: 0,
+  },
+  listensCount: {
+   type: Number,
+   default: 0,
+  },
+  owner: {
+   type: String,
+  },
+ },
+ { timestamps: true }
+);
+export const PlayListModel = mongoose.model('playlist', playListSchema);
